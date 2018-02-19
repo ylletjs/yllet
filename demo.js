8940f13c80cf4e75dff4d46523d3e7483db03860
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2018-02-19 21:54:48 +0100

    Remove demo.js

--- demo.js
+++
@@ -1,8 +0,0 @@
-import Client from './packages/yllet/src/index';
-import { objectKeysToSnakeCase } from './packages/yllet/src/util';
-
-console.log(objectKeysToSnakeCase({
-  abc: {
-    abcData: 1
-  }
-}))
