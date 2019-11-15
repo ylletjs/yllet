bf8845dce1e0e103aa406c301f8287bbf4d68320
Andrew Mclagan <andrewmclagan@gmail.com>
2019-11-15 15:30:52 +1100

    Test coverage harness

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  projects: [ '<rootDir>/packages/*/jest.config.js'],
   cacheDirectory: ".jest-cache",
+  coverageDirectory: '.jest-coverage',
+  projects: ["<rootDir>/packages/*/jest.config.js"],
   testPathIgnorePatterns: ["<rootDir>/packages/(?:.+?)/lib/"],
+  coveragePathIgnorePatterns: ["<rootDir>/packages/(?:.+?)/lib/"],
+  coverageReporters: ["html", "text"],
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95
+    }
+  }
 };
