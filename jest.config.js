06314387c673dabcd1329f9e84ce420d3e7576d8
Andrew Mclagan <andrewmclagan@gmail.com>
2019-11-15 16:46:36 +1100

    Drone configs

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,10 @@ module.exports = {
   coveragePathIgnorePatterns: ["<rootDir>/packages/(?:.+?)/lib/"],
   coverageThreshold: {
     global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95
     }
   }
 };
