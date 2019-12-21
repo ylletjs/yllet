97142e82747db65c74c11d0b2a70bc733e078a99
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-12-21 13:19:55 +0100

    Revert "Fix format"
    This reverts commit 9d5fdaf7d069b8daf2811383487d8473e0e04b61.

--- packages/client/__mocks__/MockTransport.js
+++ packages/client/__mocks__/MockTransport.js
@@ -1,5 +1,5 @@
-import "core-js/features/promise";
-import mock from "jest-mock";
+import 'core-js/features/promise';
+import mock from 'jest-mock';
 export default class MockTransport {
   constructor(responses = {}) {
     this.responses = responses;
@@ -7,7 +7,7 @@ export default class MockTransport {
   }
 
   resetMocks() {
-    ["post", "get", "put", "patch", "delete"].forEach(verb => {
+    ['post', 'get', 'put', 'patch', 'delete'].forEach(verb => {
       this[verb] = mock.fn((url, data, config) => this.request(verb));
     });
   }
