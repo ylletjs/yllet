2a0e42f6938bd6176c734a9f3ff7d1c11797ba70
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-12-22 16:14:59 +0100

    Add missing brackets

--- packages/client/src/utils.js
+++ packages/client/src/utils.js
@@ -45,12 +45,17 @@ export const queryString = (params, prefix) => {
   const query = Object.keys(params).map(key => {
     const value = params[key];
 
-    if (params.constructor === Array) key = `${prefix}[]`;
-    else if (params.constructor === Object)
+    if (params.constructor === Array) {
+      key = `${prefix}[]`;
+    } else if (params.constructor === Object) {
       key = prefix ? `${prefix}[${key}]` : key;
+    }
 
-    if (typeof value === 'object') return queryString(value, key);
-    else return `${key}=${encodeURIComponent(value)}`;
+    if (isObject(value)) {
+      return queryString(value, key);
+    } else {
+      return `${key}=${encodeURIComponent(value)}`;
+    }
   });
 
   return [].concat.apply([], query).join('&');
