008ca94fff0e80697fd83534918886484fe58955
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-12-06 21:36:39 +0100

    Remove karma

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  rootDir: path.resolve(__dirname, '..'),
-  cacheDirectory: path.resolve(__dirname, '..', '.jest-cache'),
-  coverageDirectory: path.resolve(__dirname, '..', '.jest-coverage'),
   projects: ['<rootDir>/packages/*/jest.config.js'],
   testPathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/lib/'],
   coveragePathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/lib/'],
