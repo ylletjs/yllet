33725bacd7a8c3a63dc87f968ebb7524881425a9
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-12-06 23:51:04 +0100

    Remove coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,5 @@ module.exports = {
   projects: ['<rootDir>/packages/*/jest.config.js'],
   testPathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/lib/'],
   coveragePathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/lib/'],
-  coverageReporters: ['html', 'json', 'lcov', 'text', 'clover'],
-  coverageThreshold: {
-    global: {
-      branches: 85,
-      functions: 95,
-      lines: 95,
-      statements: 95
-    }
-  }
+  coverageReporters: ['html', 'json', 'lcov', 'text', 'clover']
 };
